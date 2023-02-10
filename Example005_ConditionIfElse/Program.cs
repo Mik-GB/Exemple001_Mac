8db70c703f75150f774c8f020a6a77b6f1fdd042
Pronin Mike <mr.mi@mail.ru>
2023-02-11 01:22:14 +0400

    Example005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+  Console.WriteLine("Ура, это же Маша!");
+}
+else
+{
+  Console.Write("Привет, ");
+  Console.WriteLine(username);
+}
