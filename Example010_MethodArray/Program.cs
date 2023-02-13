242df953b8522a65e6ba2b4ddb3d4f716b773e6c
Pronin Mike <mr.mi@mail.ru>
2023-02-14 00:24:31 +0400

    Seminar №2 all

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = { 1, 2, 3, 8, 4, 5, 6, 7, 8 };
+
+int n = array.Length;
+int find = 8;
+
+int index = 0;
+
+while (index < n)
+{
+  if (array[index] == find)
+  {
+    Console.WriteLine(index);
+    break;
+  }
+  index++;
+}
